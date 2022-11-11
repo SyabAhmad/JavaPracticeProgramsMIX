3828168eed596a22a37ac04a0a822b8e6338f26c
DEVELoper <syabblogger@gmail.com>
2022-11-11 16:26:48 +0500

    update repo

---
+++ src/Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String [] args){
+        System.out.print("Hello");
+        //System.out.print("Not Hello"); /// single line comments in java
+        System.out.print("Developer");
+        /*System.out.print("Hello");      /// Multiline comments in java
+        System.out.print("Not Hello");
+        System.out.print("Developer");*/
+    }
+}
