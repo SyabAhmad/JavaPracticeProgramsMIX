3db24e3a88d86c8f3f2030d07b6c45fa59f58048
DEVELoper <syabblogger@gmail.com>
2022-11-12 14:06:10 +0500

    update repo

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -3,8 +3,13 @@ public class dataTypes {
         ///// Data Types
         String userName = "Syed Developer";
         int age = 21;
-        float pi1;
+        float pi1 = 2;
         double pi = 3.1415;
         char char1 = 'D';
+        System.out.println(userName);
+        System.out.println(age);
+        System.out.println(pi);
+        System.out.println(pi1);
+        System.out.println(char1);
     }
 }
