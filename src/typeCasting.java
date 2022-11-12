d2f268b20726f8f379d19878aeea35032c9b0c2b
DEVELoper <syabblogger@gmail.com>
2022-11-12 14:14:13 +0500

    update repo

---
+++ src/typeCasting.java
@@ -0,0 +1,17 @@
+public class typeCasting {
+    public static void main(String[] args){
+        ///// Type Casting
+        String value = "123";
+        int number = Integer.parseInt(value);
+        System.out.println(number);
+
+        /*String value = "hi";
+        try{
+            int number = Integer.parseInt(value);
+            System.out.println(number);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }*/
+    }
+}
