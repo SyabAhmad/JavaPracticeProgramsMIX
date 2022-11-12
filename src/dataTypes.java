e6efb182ccdd51b3307fdd90def9266b8e69fed7
DEVELoper <syabblogger@gmail.com>
2022-11-12 14:05:09 +0500

    update repo

---
+++ src/dataTypes.java
@@ -0,0 +1,10 @@
+public class dataTypes {
+    public static void main(String[] args){
+        ///// Data Types
+        String userName = "Syed Developer";
+        int age = 21;
+        float pi1;
+        double pi = 3.1415;
+        char char1 = 'D';
+    }
+}
--- src/variables.java
+++ src/variables.java
@@ -1,9 +1,6 @@
 public class variables {
     public static void main(String [] args){
         String userName = "Syed Developer";
-        int age = 21;
-        float pi1;
-        double pi = 3.1415;
-        char char1 = 'D';
+        System.out.println("Name : " + userName);
     }
 }
