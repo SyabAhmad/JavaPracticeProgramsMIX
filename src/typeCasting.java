87f4c2b5225dee201375b21169e7cb4f1a0645a2
DEVELoper <syabblogger@gmail.com>
2022-11-12 14:17:14 +0500

    update repo

--- src/typeCasting.java
+++ src/typeCasting.java
@@ -4,7 +4,9 @@ public class typeCasting {
         String value = "123";
         int number = Integer.parseInt(value);
         System.out.println(number);
-
+        double number1 = Double.parseDouble(value);
+        double number2 = number;
+        /// byte number3 = number;//////It will Show Error cos we can not convert large to small
         /*String value = "hi";
         try{
             int number = Integer.parseInt(value);
